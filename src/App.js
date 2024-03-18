f53f72e2e57cddaca46e43ab434fef51f2a4ff03
volokhinn <csgomx@mail.ru>
2024-03-19 00:08:41 +0700

    Added styles for auth

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import AuthWindow from './components/AuthWindow/AuthWindow';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
+import './App.css';
 
 const App = () => {
   return (
     <Provider store={store}>
-      <div>
+      <div className="container">
         <BrowserRouter>
           <Routes>
             <Route exact path="/" element={<AuthWindow />} />
--- src/components/AuthWindow/AuthWindow.jsx
+++ src/components/AuthWindow/AuthWindow.jsx
@@ -2,6 +2,10 @@ import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { login } from '../../redux/slices/authSlice';
 
+import styles from './AuthWindow.module.scss';
+
+import logo from '../../img/iway.svg';
+
 const LoginWindow = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
@@ -11,7 +15,7 @@ const LoginWindow = () => {
   const handleLogin = () => {
     dispatch(login({ login: username, password: password })).then((result) => {
       if (login.fulfilled.match(result)) {
-        console.log('Успешный логин');
+        console.log(result.payload.result.token);
       } else if (login.rejected.match(result)) {
         const payload = result.payload;
         if (payload.error && payload.error.message) {
@@ -22,21 +26,26 @@ const LoginWindow = () => {
   };
 
   return (
-    <div>
-      <h2>Авторизация</h2>
-      <input
-        type="text"
-        placeholder="Логин"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-      />
-      <input
-        type="password"
-        placeholder="Пароль"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button onClick={handleLogin}>Войти</button>
+    <div className={styles.wrapper}>
+      <img src={logo} alt="iway" />
+      <h1 className={styles.title}>Авторизация</h1>
+      <div className={styles.inputs}>
+        <input
+          type="text"
+          placeholder="Логин"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Пароль"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <button className={styles.btn} onClick={handleLogin}>
+        Войти
+      </button>
       {error && <div style={{ color: 'red' }}>{error}</div>}
     </div>
   );
